10a3226e0c6fb88166b950f132cfe1b3b638dbf4
mbuie <mbuie@cityofmesquite.com>
2019-04-29 13:49:44 -0500

    initial commit

--- SolidWaste/IdentifyRecycleDateByAddress.py
+++ SolidWaste/IdentifyRecycleDateByAddress.py
@@ -9,8 +9,9 @@ __maintainer__ = "Mark Buie"
 __email__ = "mbuie@cityofmesquite.com"
 __status__ = "Development"
 
-
+import arcinfo
 import arcpy
+import os
 
 ########################################################################################################################
 #
@@ -18,14 +19,10 @@ import arcpy
 #
 ########################################################################################################################
 
-save_path = arcpy.GetParameterAsText(0)
-in_spatialref = arcpy.GetParameterAsText(1)
-in_tax_layer = arcpy.GetParameterAsText(2)
-in_zone_layer = arcpy.GetParameterAsText(3)
-in_zone_field = arcpy.GetParameterAsText(4)
-in_grid_layer = arcpy.GetParameterAsText(5)
-in_grid_field = arcpy.GetParameterAsText(6)
-
+addressLayer = arcpy.GetParameterAsText(0)
+recycleLayer = arcpy.GetParameterAsText(1)
+addressString = arcpy.GetParameterAsText(2)
+# # outFeature = arcpy.GetParameterAsText(3)
 
 ########################################################################################################################
 #
@@ -39,17 +36,58 @@ in_grid_field = arcpy.GetParameterAsText(6)
 #
 ########################################################################################################################
 
+output_path = arcpy.env.scratchGDB
 ########################################################################################################################
 #
 #                                                     VARIALBES
 #
 ########################################################################################################################
 
+address_SQL = '"MUNIS_CLAS" NOT IN(\'UTILITY_ADDRESS\', \'OUTSIDE_CITY\') ' \
+              'AND "MESQ_CLASS" NOT IN (\'OUTSIDE_CITY\', \'OUTSIDE_CITY_MISD\', \'OUTSIDE_CITY_MISD_TAX\') ' \
+              'AND "ADDRESS" LIKE '
+input_address_SQL = '\'%' + addressString + '%\''
+where_clause = address_SQL + input_address_SQL
+
+output_fields = ['ADDRESS', 'FID_MESQ_GARB_ROTO_RECYCLE', 'FID_MESQ_ROTO_BOOM', 'DAY',
+                 'ROUTE', 'FID_MESQ_GARBAGE_COLLECTION', 'GCDAREA', 'FID_MESQ_RECYCLING',
+                 'RCDAREA']
+
 ########################################################################################################################
 #
 #                                                  SCRIPT
 #
 ########################################################################################################################
+
+arcpy.AddMessage("creating address_lyr")
+address_lyr = arcpy.MakeFeatureLayer_management(addressLayer, "#", where_clause)
+
+arcpy.AddMessage("creating intersect")
+intersect = arcpy.Intersect_analysis([address_lyr, recycleLayer], "#")
+
+arcpy.AddMessage("creating intersect_lyr")
+intersect_lyr = arcpy.MakeFeatureLayer_management(intersect, "#")
+
+arcpy.AddMessage("removing unwated fields")
+desc = arcpy.Describe(intersect_lyr)
+arcpy.AddMessage(desc.dataType)
+field_info = desc.fieldInfo
+for i in range(0, field_info.count):
+    if field_info.getFieldName(i) not in output_fields:
+        field_info.setVisible(i, "HIDDEN")
+
+arcpy.AddMessage("creating result_lyr")
+result_lyr = arcpy.MakeFeatureLayer_management(intersect_lyr, "#", "", "", field_info)
+#
+# arcpy.AddMessage("creating result_fc")
+# arcpy.CopyFeatures_management(result_lyr, "output")
+
+arcpy.FeatureClassToFeatureClass_conversion(result_lyr, output_path, "result_lyr")
+
+arcpy.AddMessage("generating output")
+arcpy.SetParameterAsText(3, os.path.join(output_path, "result_lyr"))
+
+
 
 ########################################################################################################################
 #
---
+++ SolidWaste/README.md
@@ -0,0 +1,6 @@
+# Identify Solid Waste Collection Date by Address
+This Python Tool is published as a Geoprocessing Service on ArcGIS Server. Once published, a request can be
+made to the REST endpoint with the address in question. The results of the request are then published as a table,
+letting the end user know their garbage and recycling collection dates. 
+
+![result](https://github.com/mebuie/mebuie.github.io/blob/master/img/github/SolidWasteGP.png)
